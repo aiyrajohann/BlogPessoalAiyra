696a2ea583b73d39a0a00ce3e4928f3d31cf0b40
Aluno Entrega <aluno@entrega.com>
2026-01-15 12:51:05 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from "./pages/home/Home";
+import Navbar from "./componentes/navbar/Navbar";
+import Footer from "./componentes/footer/Footer";
+
+function App() {
+  return (
+ <>
+  <Navbar />
+  <Home /> 
+  <Footer />
+ </>
+  );
+}
+
+export default App;
