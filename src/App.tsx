660ec85c61db1a3dec3860e45d4672f8e58b8aaf
Aluno Entrega <aluno@entrega.com>
2026-01-15 14:18:10 -0300

    Feat: Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import Home from "./pages/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./componentes/navbar/Navbar";
 import Footer from "./componentes/footer/Footer";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/login"; 
+import Cadastro from "./pages/cadastro/cadastro";
+
 
 function App() {
   return (
  <>
+ <BrowserRouter>    
   <Navbar />
-  <Home /> 
+  <div className="min-h-[80vh]">
+    <Routes>
+      <Route path="/" element={<Login />} />  
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Cadastro" element={<Cadastro />} />
+    </Routes>
+  </div>  
   <Footer />
+ </BrowserRouter>
  </>
-  );
+  )
 }
 
 export default App;
--- src/componentes/navbar/Navbar.tsx
+++ src/componentes/navbar/Navbar.tsx
@@ -1,18 +1,21 @@
-function Navbar() {
-  return (
-    <>
-      <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
-        <div className="container flex justify-between text-lg mx-8">
-          Blog Pessoal Aiyra Johann
+import { Link } from 'react-router-dom'
 
-          <div className="flex gap-4">
-            Postagens
-            Perfil
-          </div>
-        </div>
-      </div>
-    </>
-  )
+function Navbar() {
+    return (
+        <>
+            <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
+                <div className="container flex justify-between text-lg mx-8">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+                    <div className="flex gap-4">
+                        Postagens
+                        Perfil
+                        Cadastro
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
